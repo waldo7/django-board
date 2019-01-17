fbd288e432c21205d64f75b45420d57e5793a216
Waldo Ramones <waldo.ramones@gmail.com>
2019-01-17 12:17:41 +0800

    Setup board page

--- boards/tests.py
+++ boards/tests.py
@@ -1,3 +1,17 @@
 from django.test import TestCase
+from django.urls import reverse, resolve
 
+from .views import HomeView
 # Create your tests here.
+
+
+class HomeTests(TestCase):
+    def test_home_view_status_code(self):
+        url = reverse('home')
+        response = self.client.get(url)
+        self.assertEquals(response.status_code, 200)
+
+    def test_home_url_resolve_home_view(self):
+        view = resolve('/')
+        self.assertEquals(view.view_name, 'home')
+        # self.assertEquals(view.func.view_class, HomeView)
--- boards/views.py
+++ boards/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.views.generic import ListView
 
+from .models import Board
 # Create your views here.
+
+
+def home(request):
+    return HttpResponse("hello")
+
+
+class HomeView(ListView):
+    model = Board
+    context_object_name = "boards"
+    template_name = "boards/home.html"
