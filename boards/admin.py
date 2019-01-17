eacd91c57f8672a3f8d5901d1b1e832cfe46cc9f
Waldo Ramones <waldo.ramones@gmail.com>
2019-01-17 12:24:32 +0800

    Setup admin page

--- boards/admin.py
+++ boards/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
+from .models import Board
 # Register your models here.
+
+
+class BoardAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'description')
+
+
+admin.site.register(Board, BoardAdmin)
